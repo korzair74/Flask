caf57856ea05dacd00ed447205191a5ee10b22b9
Chris Nickel <chris.nickel.dev@gmail.com>
2020-04-24 16:38:47 -0400

    sqlite db

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
 app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    return "Hey Flask"
-
-if __name__ == "__main__":
-  app.run(debug=True)
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+class Guide(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), unique=False)
+    content = db.Column(db.String(144), unique=False)
+    def __init__(self, title, content):
+        self.title = title
+        self.content = content
+class GuideScheme(ma.Schema):
+    class Meta:
+        fields = ('title', 'content')
+guide_schema = GuideScheme()
+guide_schemas = GuideScheme(many=True)
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
