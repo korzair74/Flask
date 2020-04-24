2d4f3a5ea1c2ebf9a25ac85392501adaaedfc383
Chris Nickel <chris.nickel.dev@gmail.com>
2020-04-24 18:52:20 -0400

    Delete guide and show status

--- app.py
+++ app.py
@@ -60,6 +60,15 @@ def guide_update(id):
   db.session.commit()
   return guide_schema.jsonify(guide)
 
+@app.route('/guide/<id>', methods=["DELETE"])
+def guide_delete(id):
+  guide = Guide.query.get(id)
+  db.session.delete(guide)
+  db.session.commit()
+  return "Guide was successfully Deleted"
+
+  return
+
 if __name__ == '__main__':
     app.run(debug=True)
 
